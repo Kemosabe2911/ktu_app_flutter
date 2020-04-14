b4644a83a56bbebf62cad84c61dd8ae1a5f61e90
Kemosabe2911 <stevinprince29@gmail.com>
2020-04-14 16:00:38 +0530

    Initial commit

---
+++ ktu_help/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.black),
+      home: Scaffold(
+            appBar: AppBar(title: Text('KTU HELP',style: TextStyle(fontSize: 20.0))
+      )
+    ); 
+  }
+}
