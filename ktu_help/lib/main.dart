fed7552e115192ae3268e80e1b45feb7d139f4b3
Kemosabe2911 <stevinprince29@gmail.com>
2020-04-16 11:56:07 +0530

    Second Commit

--- ktu_help/lib/main.dart
+++ ktu_help/lib/main.dart
@@ -4,13 +4,28 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  var _courses= ['CSE','ME','CE','ECE','EEE'];
+  VAR _courseSelected='Select Course';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.black),
       home: Scaffold(
             appBar: AppBar(title: Text('KTU HELP',style: TextStyle(fontSize: 20.0))
-      )
+      ),
+      body: Container(
+        child: Text('Select Course:',style: TextStyle(fontSize: 20.0)),
+        DropdownButton<String>(
+          items: _courses.map((String dropDownStringItem){
+            return DropdownMenuItem<String>(
+              value: dropDownStringItem,
+              child: Text(dropDownStringItem),
+            );
+          }).toList(),
+
+          
+        )
+      ),
     ); 
   }
 }
