e6d3842f53975f7eea792e9bcaf1eafb6c25ef93
Kemosabe2911 <stevinprince29@gmail.com>
2020-04-19 19:52:26 +0530

    Update main.dart

--- ktu_help/lib/main.dart
+++ ktu_help/lib/main.dart
@@ -3,55 +3,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefullWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.black),
       home: Scaffold(
-            appBar: AppBar(title: Text('KTU HELP',style: TextStyle(fontSize: 20.0))
-      ),
-      body: Center(
-          child: MyStatefulWidget(),
-        ),
-    )); 
+        
+      )); 
   }
 }
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
 
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  String dropdownValue = 'Select Course';
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 16,
-      style: TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String newValue) {
-        setState(() {
-          dropdownValue = newValue;
-        });
-      },
-      items: <String>['CSE','ECE','ME','CE','EEE']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    );
-  }
-}
