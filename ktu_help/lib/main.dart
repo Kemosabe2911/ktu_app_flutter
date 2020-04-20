3fff6165a3a71251fe717161d8e3fb52fc504d50
Kemosabe2911 <stevinprince29@gmail.com>
2020-04-20 20:43:45 +0530

    Add tabs

--- ktu_help/lib/main.dart
+++ ktu_help/lib/main.dart
@@ -13,12 +13,12 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
         primaryColor: Colors.black
       ),
-      home: MyStatefulWidget(),
+      home: MyTabbedPage(),
       debugShowCheckedModeBanner: false,
       ); 
   }
 }
-class MyStatefulWidget extends StatefulWidget{
+/*class MyStatefulWidget extends StatefulWidget{
   @override
   MyStatefulWidgetState createState() => MyStatefulWidgetState();
 }
@@ -26,8 +26,59 @@ class MyStatefulWidget extends StatefulWidget{
 class MyStatefulWidgetState extends State<MyStatefulWidget>{
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text('KTU HELP',style: TextStyle(fontSize: 22.0),),
-      actions: <Widget>[IconButton(icon: Icon(Icons.list), onPressed: null)],),
+      appBar: MyTabbedPage(),
+    );
+  }
+}*/
+class MyTabbedPage extends StatefulWidget {
+  const MyTabbedPage({ Key key }) : super(key: key);
+  @override
+  _MyTabbedPageState createState() => _MyTabbedPageState();
+}
+
+class _MyTabbedPageState extends State<MyTabbedPage> with SingleTickerProviderStateMixin {
+  final List<Tab> myTabs = <Tab>[
+    Tab(text: 'CGPA'),
+    Tab(text: 'SGPA'),
+  ];
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: myTabs.length);
+  }
+
+ @override
+ void dispose() {
+   _tabController.dispose();
+   super.dispose();
+ }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('KTU HELP',style: TextStyle(fontSize: 22.0),),
+        actions: <Widget>[IconButton(icon: Icon(Icons.list), onPressed: null)],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: myTabs,
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: myTabs.map((Tab tab) {
+          final String label = tab.text.toLowerCase();
+          return Center(
+            child: Text(
+              'This is the $label tab',
+              style: const TextStyle(fontSize: 36),
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
