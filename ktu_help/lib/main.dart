84f669d023999de21e3c16761f3dedd2ee2f541c
Kemosabe2911 <stevinprince29@gmail.com>
2020-04-20 20:24:08 +0530

    Add stateful widget

--- ktu_help/lib/main.dart
+++ ktu_help/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,11 +13,21 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
         primaryColor: Colors.black
       ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('KTU HELP')),
-      ),
+      home: MyStatefulWidget(),
       debugShowCheckedModeBanner: false,
       ); 
+  }
+}
+class MyStatefulWidget extends StatefulWidget{
+  @override
+  MyStatefulWidgetState createState() => MyStatefulWidgetState();
+}
+
+class MyStatefulWidgetState extends State<MyStatefulWidget>{
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text('KTU HELP',style: TextStyle(fontSize: 22.0),)),
+    );
   }
 }
 
