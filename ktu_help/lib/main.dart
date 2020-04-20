0f25eb010dee894206a84da2dd49bc2c7a1ff842
Kemosabe2911 <stevinprince29@gmail.com>
2020-04-20 19:48:47 +0530

    Add theme

--- ktu_help/lib/main.dart
+++ ktu_help/lib/main.dart
@@ -3,14 +3,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 
-class MyApp extends StatefullWidget {
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black
+      ),
       home: Scaffold(
-        
-      )); 
+        appBar: AppBar(title: Text('KTU HELP')),
+      ),
+      debugShowCheckedModeBanner: false,
+      ); 
   }
 }
 
