c528f665b1dee6d5798475ae76d9939dfc1bbbae
Kemosabe2911 <stevinprince29@gmail.com>
2020-04-25 19:07:54 +0530

    Add drawer

--- ktu_help/lib/main.dart
+++ ktu_help/lib/main.dart
@@ -60,8 +60,7 @@ class _MyTabbedPageState extends State<MyTabbedPage> with SingleTickerProviderSt
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('KTU HELP',style: TextStyle(fontSize: 22.0),),
-        actions: <Widget>[IconButton(icon: Icon(Icons.list), onPressed: null)],
+        title: Text('KTU HELP',style: TextStyle(fontSize: 24.0),),
         bottom: TabBar(
           controller: _tabController,
           tabs: myTabs,
@@ -79,7 +78,44 @@ class _MyTabbedPageState extends State<MyTabbedPage> with SingleTickerProviderSt
           );
         }).toList(),
       ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('KTU HELP'),
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
 
